52ccc83495a265bc807592d601cb70d020d3a210
Kelly Miller <kellyannemiller@gmail.com>
2017-07-13 21:57:31 -0600

    configure circleci and heroku

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,5 @@
 module.exports = {
+
   development: {
     client: 'pg',
     connection: 'postgres://localhost/byob',
@@ -6,7 +7,25 @@ module.exports = {
       directory: './db/migrations'
     },
     seeds: {
-      directory: './db/seeds/dev'
+     directory: './db/seeds/dev'
+    },
+    useNullAsDefault: true
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    migrations: {
+      directory: './db/migrations'
+    },
+    useNullAsDefault: true
+  },
+
+  staging: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    migrations: {
+      directory: './db/migrations'
     },
     useNullAsDefault: true
   },
