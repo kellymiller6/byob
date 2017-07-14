45f0efc07c414b4566711d9431763a41987e6043
Kelly Miller <kellyannemiller@gmail.com>
2017-07-14 14:50:06 -0600

    change knex file

--- knexfile.js
+++ knexfile.js
@@ -12,24 +12,6 @@ module.exports = {
     useNullAsDefault: true
   },
 
-  production: {
-    client: 'pg',
-    connection: process.env.DATABASE_URL + `?ssl=true`,
-    migrations: {
-      directory: './db/migrations'
-    },
-    useNullAsDefault: true
-  },
-
-  staging: {
-    client: 'pg',
-    connection: process.env.DATABASE_URL + `?ssl=true`,
-    migrations: {
-      directory: './db/migrations'
-    },
-    useNullAsDefault: true
-  },
-
   test: {
     client: 'pg',
     connection: process.env.DATABASE_URL || 'postgres://localhost/byob_test',
@@ -38,6 +20,18 @@ module.exports = {
     },
     seeds: {
       directory: './db/seeds/test'
+    },
+    useNullAsDefault: true
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+     directory: './db/seeds/dev'
     },
     useNullAsDefault: true
   }
